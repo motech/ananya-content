0f2d249e097fc500c888e37f2ba6f2613e77a9a8
Rahul Garg <rgarg@thoughtworks.com>
2012-02-09 11:37:29 +0530

    sneha, rahul | #1190 | added script and tool(sox) to trim audio files for testing

---
+++ src/main/resources/trim_audio_files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script trims first and last n seconds of a wav file and appends then to create a shorter 2n length wav file.
+# It converts all the .wav files(except for one with "prompt" in their name) in the "audio" folder
+
+audioDir=`pwd`"/audio"
+audioTestDir=$audioDir" test"
+soxDir="tools/sox-14-3-2"
+
+IFS=$'\n'
+rm -rf "$audioTestDir"
+cp -r "$audioDir" "$audioTestDir"
+cd "$soxDir"
+
+for f in `find "$audioTestDir" -type f |grep .*.wav | grep -v .*prompt.*.wav`; do
+  LEN=`sox "$f" -n stat 2>&1 | sed -n 's#^Length (seconds):[^0-9]*\([0-9.]*\)$#\1#p'`
+  LEN=${LEN/.*}
+  if [ ! -z $LEN ] && [ $LEN -gt 20 ]; then
+	LAST=$(($LEN - 10))
+	sox.exe "$f" first.wav trim 0 0:10
+    sox.exe "$f" last.wav trim $LAST $LEN
+	sox.exe first.wav last.wav "$f"
+  fi
+done
+rm -f first.wav
+rm -f last.wav
